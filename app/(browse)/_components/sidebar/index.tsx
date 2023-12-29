c7fb8e62912733f0bb1cec5fa8627f6d685838e7
Prashant <prashantco111@gmail.com>
2023-12-29 12:51:41 +0530

    Add ToggleSkeleton and RecommendedSkeleton components to SidebarSkeleton

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { getRecommended } from '@/lib/recommendedService'
 import { Recommended, RecommendedSkeleton } from './recommended'
-import { Toggle } from './toggle'
+import { Toggle, ToggleSkeleton } from './toggle'
 import { Wrapper } from './wrapper'
 
 export const Sidebar = async () => {
@@ -20,6 +20,7 @@ export const SidebarSkeleton = () => {
 	return (
 		<aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
 			<div className="space-y-4 pt-4 lg:pt-5">
+				<ToggleSkeleton />
 				<RecommendedSkeleton />
 			</div>
 		</aside>
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,17 +1,36 @@
 'use client'
 import { cn } from '@/lib/utils'
 import { useSidebar } from '@/store/useSidebar'
+import { useEffect, useState } from 'react'
+import { ToggleSkeleton } from './toggle'
+import { RecommendedSkeleton } from './recommended'
 
 interface WrapperProps {
 	children: React.ReactNode
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+	const [isClient, setIsClient] = useState(false)
+
 	const { collapsed } = useSidebar((state) => state)
+
+	useEffect(() => {
+		setIsClient(true)
+	}, [])
+
+	// to prevent flickering on client side
+	if (!isClient)
+		return (
+			<aside className="'fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-35'">
+				<ToggleSkeleton />
+				<RecommendedSkeleton />
+			</aside>
+		)
+
 	return (
 		<aside
 			className={cn(
-				'fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-35',
+				'fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-35',
 				collapsed && 'w-[70px]'
 			)}>
 			{children}
